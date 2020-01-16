e9f99746cf5ea6ade9a97a4c5ce5b665eba5083f
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-16 14:43:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,30 @@
 def starts_with_a_vowel?(word)
-
+  arr = word.scan(/^[aeiou]/i)
+  if arr.length > 0
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  arr = text.scan(/un.*?ing/)
+  #arr = text.scan(/un\..*ing/)
+  return arr
 end
 
 def words_five_letters_long(text)
-
+  arr = text.scan(/\b\w{5}\b/)
+  return arr
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ arr = text.scan(/^[A-Z].*?[.,?]/)
+ if arr > 0
+   return true
+ else
+   return false
+ end
 end
 
 def valid_phone_number?(phone)
